96bf73fe67c46511bec82654dde987103c53bb20
Kit Sunde <kitsunde@gmail.com>
2017-10-25 02:41:59 +0200

    Allow users to register in the dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,20 +1,30 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  session: service(),
+  firebaseApp: service(),
 
-    actions: {
-        login: function() {
-            this.get('session').authenticate('authenticator:firebase', {
-                'email': this.get('email'),
-                'password': this.get('password')
-            }).then(function() {
-                this.transitionToRoute('index');
-            }.bind(this));
-        },
-        logout: function() {
-            this.get('session').invalidate().then(function() {
-                this.transitionToRoute('login');
-            }.bind(this));
-        }
+  actions: {
+    login() {
+      this.get('session').authenticate('authenticator:firebase', {
+        'email': this.get('email'),
+        'password': this.get('password')
+      }).then(() => {
+        this.transitionToRoute('index');
+      });
+    },
+    register() {
+      this.get('firebaseApp').auth().createUserWithEmailAndPassword(
+        this.get('registerEmail'), this.get('registerPassword')
+      ).then(() => {
+        this.transitionToRoute('index');
+      });
+    },
+    logout() {
+      this.get('session').invalidate().then(() => {
+        this.transitionToRoute('login');
+      });
     }
+  }
 });
